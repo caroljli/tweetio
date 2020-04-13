280cb485945cd605fad41843bd932fde9fa36e5b
Carol Li <caroldotli@gmail.com>
2020-04-12 22:49:42 -0400

    html layout update

--- tweetio/tweets/models.py
+++ tweetio/tweets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# account class extends user class
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200)
+    bio = models.TextField()
+
+class Tweet(models.Model):
+    # TODO: implement author
+    time = models.DateTimeField()
+    body = models.TextField()
+    liked = False
--- tweetio/tweets/views.py
+++ tweetio/tweets/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from tweets.models import Account
 
 def splash(request):
 	return render(request, "splash.html", {})
@@ -6,20 +9,41 @@ def splash(request):
 def home(request):
 	return render(request, "home.html", {})
 
-def login(request):
-    return render(request, "login.html", {})
-
-def register(request):
-    return render(request, "register.html", {})
-
 def profile(request):
     return render(request, "profile.html", {})
 
 def self(request):
-    return render(request, "self.html", {})
+    account = Account.objects.all()
+    return render(request, "self.html", {"account" : account})
 
 def hashtag(request):
     return render(request, "hashtag.html", {})
 
 def register_complete(request):
     return render(request, "register-complete.html", {})
+
+# USER AUTHENTICATION
+
+# redirects to self
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/self")
+    return render(request, "login.html", {})
+
+def logout_(request):
+    logout(request)
+    return redirect("/")
+
+def register(request):
+    user = User.objects.create_user(username=request.POST['username'],
+                                    email=request.POST['email'],
+                                    password=request.POST['password'])
+    return redirect('/register-complete')
+
+def register(request):
+    return render(request, "register.html", {})
